8d69b5072df4129eee1889143f8a75f6ad753bfb
Lauren Premer Lee <58481766+LaurenLee30@users.noreply.github.com>
2024-02-13 20:22:05 -0600

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,14 @@
+<button onclick="two()">2</button>
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a "loop" variable
+var i;
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "50%";
+  }
+}
